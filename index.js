0d6fc33da8f14898ffd26abe976e616ea2dfa152
newell212 <michaelnewell@michaels-air.lan>
2022-08-20 11:25:26 -0600

    created function is31Flavors

--- index.js
+++ index.js
@@ -63,10 +63,14 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-  /*your code here*/
+function is31Flavors(){
+  if(originalFlavors.length === 31){
+    return "true"
+  } else{
+    return "false"
+  }
  }
-
+console.log(is31Flavors());
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
